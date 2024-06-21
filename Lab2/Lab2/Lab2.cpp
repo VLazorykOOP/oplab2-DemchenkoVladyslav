482527596d8b0bfe5a42f4ae411b1f74afbcbc9e
DemchenkoVladyslav <demchenkovlad80@gmail.com>
2024-06-22 02:09:02 +0300

    Task completed

--- Lab2/Lab2/Lab2.cpp
+++ Lab2/Lab2/Lab2.cpp
@@ -1,20 +1,152 @@
-﻿// Lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <chrono>
+#include <random>
+#include <windows.h>
+using namespace std;
+// Загальні налаштування
+const int AREA_WIDTH = 100;
+const int AREA_HEIGHT = 25;
+const int NUM_RABBITS = 10;
+const int NUM_ALBINOS = 7;
+const int CHANGE_DIRECTION_INTERVAL = 3; 
+const int VELOCITY = 1;
 
-#include <iostream>
+mutex mtx;
 
-int main()
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<> dis(0, 3);
+
+class Rabbit {
+public:
+	int x, y;
+
+};
+class Albino :public Rabbit
 {
-    std::cout << "Hello World!\n";
+public:
+	bool movement = 1;
+};
+
+void moveRabbit(Rabbit& rab) {
+	while (true) {
+		this_thread::sleep_for(chrono::milliseconds(CHANGE_DIRECTION_INTERVAL * 100));
+
+		int direction = dis(gen);
+		mtx.lock();		
+		COORD c;
+		c.X = rab.x;
+		c.Y = rab.y;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+		cout << " ";
+
+		switch (direction) {
+		case 0: 
+			rab.x = min(rab.x + VELOCITY, AREA_WIDTH - 1);
+			break;
+		case 1:
+			rab.x = max(rab.x - VELOCITY, 0);
+			break;
+		case 2: 
+			rab.y = max(rab.y - VELOCITY, 0);
+			break;
+		case 3: 
+			rab.y = min(rab.y + VELOCITY, AREA_HEIGHT - 1);
+			break;
+		}
+		c.X = rab.x;
+		c.Y = rab.y;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+		cout << "R";
+		mtx.unlock();
+
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void moveAlbino(Albino& rab)
+{
+	while (true) {
+		this_thread::sleep_for(chrono::milliseconds(100));
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+		mtx.lock();
+		COORD c;
+		c.X = rab.x;
+		c.Y = rab.y;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+		cout << " ";
+		if ((rab.x == 0 && !rab.movement) || (rab.x == 99 && rab.movement))
+			rab.movement = !rab.movement;
+		rab.x = (rab.x + (rab.movement ? VELOCITY : -VELOCITY));
+		c.X = rab.x;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+		cout << "A";
+		mtx.unlock();
+	}
+}
+void setCursorrabition(int x, int y) {
+	cout << "\033[" << y + 1 << ";" << x + 1 << "H";
+}
+void setColor(int color) {
+	cout << "\033[" << color << "m";
+}
+void drawBoundary() {
+	setColor(31);
+	for (int i = 0; i <= AREA_WIDTH; ++i) {
+
+		setCursorrabition(i, AREA_HEIGHT);
+		cout << "#";
+	}
+	for (int i = 0; i <= AREA_HEIGHT; ++i) {
+
+		setCursorrabition(AREA_WIDTH, i);
+		cout << "#";
+	}
+	setColor(0);
+}
+int main() {
+	srand(time(NULL));
+	drawBoundary();
+	vector<Rabbit> rabbits(NUM_RABBITS);
+	vector<Albino> albinos(NUM_ALBINOS);
+
+	for (int i = 0; i < NUM_RABBITS; ++i)
+	{
+		rabbits[i].x = rand() % AREA_WIDTH;
+		rabbits[i].y = rand() % AREA_HEIGHT;
+		COORD c;
+		c.X = rabbits[i].x;
+		c.Y = rabbits[i].y;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+		cout << "R";
+	}
+	for (int i = 0; i < NUM_ALBINOS; ++i)
+	{
+		albinos[i].x = rand() % AREA_WIDTH;
+		albinos[i].y = rand() % AREA_HEIGHT;
+		COORD c;
+		c.X = albinos[i].x;
+		c.Y = albinos[i].y;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+		cout << "A";
+	}
+	vector< thread> rabbitThreads;
+	for (int i = 0; i < NUM_RABBITS; ++i) {
+		rabbitThreads.emplace_back(moveRabbit, ref(rabbits[i]));
+	}
+	vector< thread> albinoThreads;
+	for (int i = 0; i < NUM_ALBINOS; ++i) {
+		albinoThreads.emplace_back(moveAlbino, ref(albinos[i]));
+	}
+
+	for (auto& thread : rabbitThreads) {
+		thread.join();
+	}
+	for (auto& thread : albinoThreads) {
+		thread.join();
+	}
+
+	return 0;
+}
